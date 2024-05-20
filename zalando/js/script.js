7d2fcdfeeb819b3335f9475df6f9b7a8bfd459e3
peppepugliese <gp55759@gmail.com>
2024-05-20 23:07:39 +0200

    Add files via upload

---
+++ zalando/js/script.js
@@ -0,0 +1,18 @@
+const banner = document.getElementById('banner-off');
+banner.style.display = 'none';
+
+const cross = getElementById by
+
+setTimeout(() => {
+    banner.style.display = 'block';
+}, 5000);
+let minutes = 10;
+let seconds = 0;
+
+const interval = setInterval(() => {
+    seconds = 59;
+    minutes--;
+}
+
+minute.Print()
+)
